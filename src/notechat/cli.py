884ae2938832ae4f10b2ec2d63b917e53d739288
Mykal Machon <mykal@tinybox.dev>
2023-12-04 21:06:18 -0800

    feat: reworking CLI file

--- src/notechat/cli.py
+++ src/notechat/cli.py
@@ -16,24 +16,34 @@ import typer
 
 app = typer.Typer()
 
-
 def version_callback(value: bool):
     if value:
         typer.echo(f"{__app_name__} version {__version__}")
         raise typer.Exit()
 
 
-@app.command()
-def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        callback=version_callback,
-        is_eager=True,
-        help="Print the version and exit",
-    ),
-):
-    """This is the main entrypoint into the app"""
+@app.callback()
+def main(version: bool = typer.Option(None, "--version", "-v", is_eager=True, help="Print the version and exit", callback=version_callback)):
+    """
+    Main function to handle version flag.
+    """
     if version:
         typer.echo(f"{__app_name__} version {__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def config(): 
+    """
+    Configure NoteChat settings.
+    """
+    typer.echo("config: todo")
+    typer.Exit()
+
+@app.command()
+def chat():
+    """
+    Chat with your notes.
+    """
+    typer.echo("query: todo")
+    typer.Exit()
